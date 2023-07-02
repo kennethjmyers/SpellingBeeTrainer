4eab05950c4865555519779ac3a24b2f53266630
kennethjmyers <myers.kenneth.james@gmail.com>
2023-07-02 02:36:43 -0400

    style and readme updates

--- README.md
+++ README.md
@@ -1,15 +1,26 @@
-# <img src="public/icons/icon_48.png" width="45" align="left"> Spellingbeetrainer
+# <img src="public/icons/icon_48.png" width="45" align="left"> Spelling Bee Trainer
 
-My Chrome Extension
+A Chrome Extension that helps you learn the words you miss in the NYT Spelling Bee game.
 
 ## Features
 
-- Feature 1
-- Feature 2
+- Get the definitions for words you would like to study. Words are separated by those you found in a game and those you failed to find.
+- Planned: Vocab game - learn words via fill in the blank
 
 ## Install
 
 [**Chrome** extension]()
+
+## How to use
+
+1. Open up a Spelling Bee game from a previous day. 
+2. Reveal the answers 
+3. With the answers shown click on the extension button. This will open a new tab with the application.
+
+<img src="./images/nyt-game-example.png" style="display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 60%;">
 
 ## Contribution
 
@@ -18,4 +29,7 @@ Suggestions and pull requests are welcomed!.
 ---
 
 This project was bootstrapped with [Chrome Extension CLI](https://github.com/dutiyesh/chrome-extension-cli)
+
+
+Bee Emoji designed by [OpenMoji](https://openmoji.org/) – the open-source emoji and icon project. License: [CC BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/#)
 
---
+++ public/trainer.js
@@ -0,0 +1,185 @@
+function appendList(wordList, idToAppend, chkName) {
+  var someDiv = document.getElementById(idToAppend);
+  for (let i = 0; i < wordList.length; i++) {
+    var li = document.createElement('li');
+    var checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+    checkbox.value = wordList[i];
+    checkbox.name = chkName;
+    if (i < 3) checkbox.checked=true;
+    var s = document.createElement('span');
+    s.innerText =  wordList[i];
+
+    li.appendChild(checkbox);
+    li.appendChild(s);
+    someDiv.appendChild(li);
+  }
+}
+
+function selectAll (chkName) {
+  var ele=document.getElementsByName(chkName);  
+  for(var i=0; i<ele.length; i++){  
+    if(ele[i].type=='checkbox')  
+      ele[i].checked=true;  
+  }  
+}
+function deselectAll(chkName) {
+  var ele=document.getElementsByName(chkName);  
+  for(var i=0; i<ele.length; i++){  
+    if(ele[i].type=='checkbox')  
+      ele[i].checked=false;  
+        
+  }  
+}
+
+async function getDictResults(checkBoxValues) {
+  var dictResults = []
+  console.log(checkBoxValues.entries().next())
+  for (const word of checkBoxValues) {
+    console.log(word)
+    var baseURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    var mwResponse = await fetch(baseURL)
+    var mwJson = await mwResponse.json()
+    dictResults.push(mwJson)
+  }
+  return dictResults
+}
+
+async function getDefinitions () {
+  // clear current definitions
+  clearDefinitionCards();
+
+  // get all items that are checked
+  var checkedBoxes = document.querySelectorAll('input:checked');
+  var checkBoxValues = [];
+  checkedBoxes.forEach(x => checkBoxValues.push(x.value));
+  console.log("Checked Boxes: " + checkBoxValues);
+  // run that through dictionary api
+  var dictResults = await getDictResults(checkBoxValues);
+  console.log("all dict results")
+  console.log(dictResults)
+
+  // generate cards for the words and definitions
+  for await (const wordJson of dictResults) {
+    console.log(wordJson);
+    makeDefinitionCard(wordJson);
+  }  
+
+  var defBoxes = document.querySelectorAll(".definition-box");
+  defBoxes[0].classList.add('first-def-box');
+  defBoxes[defBoxes.length-1].classList.add('last-def-box');
+}
+
+function clearDefinitionCards () {
+  /**
+   * Finds all of the defintion-box divs and removes them from the document
+   */
+  var defBoxes = document.querySelectorAll('.definition-box');
+  defBoxes.forEach((box) => box.remove());
+}
+
+function makeDefinitionCard(wordJson) {
+  try {
+    var word = wordJson[0]['word'];
+  }
+  catch (exception) {
+    // improve handling later
+    return;
+  }
+  let meanings = wordJson[0]['meanings'][0];  // get only first meaning, could be multiple
+  let definitions = meanings['definitions'];
+  let pos = meanings['partOfSpeech'];
+  let synonyms = meanings['synonyms'];
+  let antonyms = meanings['antonyms'];
+
+  console.log(word, meanings, definitions, synonyms, antonyms);
+  
+  var synString = synonyms.join(', ');
+  var antString  = antonyms.join(', ');
+
+  // create new div to add
+  var defDiv = document.createElement('div');
+  defDiv.classList.add('definition-box');
+  
+  // could have made this more robust but I didn't plan to include more than the first 3 definitions
+  var def1 = getDefinitionFromList(definitions, 0)
+  var def2 = getDefinitionFromList(definitions, 1)
+  var def3 = getDefinitionFromList(definitions, 2)
+
+  // add content to div
+  defDiv.innerHTML = `
+        <div class="def-header">
+          <span class="def-word">${word}</span>
+          <span class="def-pos">${pos}</span>
+        </div>
+        <div class="definitions">
+          <span>${def1}</span>
+          <span>${def2}</span>
+          <span>${def3}</span>
+        </div>
+        <br>
+        <div class="synonyms">
+          <span>Synonyms: ${synString}</span>
+        </div>
+        <br>
+        <div class="antonyms">
+          <span>Antonyms: ${antString}</span>
+        </div>
+  `
+  
+  // append to the scroll window
+  var defScroll = document.querySelector("div.def-scroll");
+  defScroll.appendChild( defDiv );
+}
+
+function getDefinitionFromList (definitionList, index) {
+  try {
+    return `${index+1}. ${definitionList[index]['definition']}`;
+  } catch (exception) {
+    return '';
+  }
+}
+
+async function getData () {
+  var result;
+  await chrome.storage.local.get(["spWordsData"]).then((response) => {
+    console.log("Value currently is: ");
+    console.log(response);
+    result = response;
+  });
+  return result;
+}
+
+// In chrome extension inline scripting not allowed so need to join listeners here
+window.onload = async function () {
+  document.getElementById("knownWords-selectAll").addEventListener("click", function() {selectAll("knownWords-chk")});
+  document.getElementById("knownWords-deselectAll").addEventListener("click", function () {deselectAll("knownWords-chk")});
+  document.getElementById("unknownWords-selectAll").addEventListener("click", function () {selectAll("unknownWords-chk")});
+  document.getElementById("unknownWords-deselectAll").addEventListener("click", function () {deselectAll("unknownWords-chk")});
+  document.getElementById("getDefinitions-button").addEventListener("click", getDefinitions);
+
+  // this is just defaults for prototyping/testing
+  
+  var knownWords = [];
+  var unknownWords = [];
+  try {
+    await getData().then((response) => {
+      console.log("getData response: ");
+      console.log(response);
+      knownWords = response.spWordsData.knownWords;
+      unknownWords = response.spWordsData.unknownWords;
+    });
+  }
+  catch (exception) {
+    //load defaults
+    console.log('Exception found: ')
+    console.log(exception)
+    console.log("Loading default words.")
+    knownWords = ['naively', 'alive', 'allay', 'ally', 'anal', 'anneal', 'anvil', 'avail', 'eave', 'inlay', 'lain', 'lane', 'lava', 'lave', 'lean', 'leave', 'leaven', 'nail', 'naive', 'nana', 'nanny', 'naval', 'nave', 'navy', 'vain', 'vainly', 'vale', 'valley', 'vane', 'vanilla', 'veal', 'venal', 'venally', 'vial', 'villa', 'villain', 'villainy'].sort();
+    unknownWords = ['venially', 'alien', 'allele', 'alley', 'anally', 'annal', 'avian', 'elan', 'inane', 'inanely', 'lanai', 'leanly', 'lineal', 'lineally', 'naan', 'navel', 'valve', 'vanillin', 'venial', 'villanelle'].sort();
+  }
+  console.log(knownWords.length + " " + unknownWords.length);
+  if (knownWords.length === 0 && unknownWords.length === 0) alert("No known or unkwords found. Check that you are on the NYT spelling bee site with the answers visible.");
+  appendList(knownWords, "knownWords-checkboxes", "knownWords-chk");
+  appendList(unknownWords, "unknownWords-checkboxes", "unknownWords-chk");
+}
