099cfc1a28d887042a503171b42fe89a91f95234
kennethjmyers <myers.kenneth.james@gmail.com>
2023-07-03 02:29:08 -0400

    clean up code and manifest for publishing

--- src/background.js
+++ src/background.js
@@ -27,19 +27,3 @@ chrome.action.onClicked.addListener((tab) => {
   );
   chrome.tabs.create({ url: "trainer.html" });
 });
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  console.log("The request type is " + request.type);
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
-
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
-    });
-  }
-});
--- src/contentScript.js
+++ src/contentScript.js
@@ -17,19 +17,6 @@ console.log(
   `Page title is: '${pageTitle}' - evaluated by Chrome extension's 'contentScript.js' file`
 );
 
-// Communicate with background file by sending a message
-chrome.runtime.sendMessage(
-  {
-    type: 'GREETINGS',
-    payload: {
-      message: 'Hello, my name is Con. I am from ContentScript.',
-    },
-  },
-  response => {
-    console.log(response.message);
-  }
-);
-
 // Listen for message
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.type === 'GETTABLE') {
